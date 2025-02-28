0e131c09db35c54caba49b20a61a4da7f17b7b49
Luana Vieira <luanavieira.uni@outlook.com>
2025-02-28 09:51:36 -0300

    :sparkles: feat: adicionado css e js a página

--- src/main/java/br/com/fiap/spring_mvc/dto/LivroRequest.java
+++ src/main/java/br/com/fiap/spring_mvc/dto/LivroRequest.java
@@ -2,11 +2,8 @@ package br.com.fiap.spring_mvc.dto;
 
 import br.com.fiap.spring_mvc.model.Categoria;
 import jakarta.validation.constraints.*;
-import lombok.Data;
-
 import java.math.BigDecimal;
 
-@Data
 public class LivroRequest {
         @NotBlank(message = "O título é obrigatório.")
         private String titulo;
@@ -19,4 +16,44 @@ public class LivroRequest {
         private BigDecimal preco;
         @Pattern(regexp = "^970\\d{7}$|^970\\d{10}$", message = "O ISBN deve seguir o padrão internacional.")
         private String isbn;
+
+        public String getTitulo() {
+                return titulo;
+        }
+
+        public void setTitulo(String titulo) {
+                this.titulo = titulo;
+        }
+
+        public String getAutor() {
+                return autor;
+        }
+
+        public void setAutor(String autor) {
+                this.autor = autor;
+        }
+
+        public Categoria getCategoria() {
+                return categoria;
+        }
+
+        public void setCategoria(Categoria categoria) {
+                this.categoria = categoria;
+        }
+
+        public BigDecimal getPreco() {
+                return preco;
+        }
+
+        public void setPreco(BigDecimal preco) {
+                this.preco = preco;
+        }
+
+        public String getIsbn() {
+                return isbn;
+        }
+
+        public void setIsbn(String isbn) {
+                this.isbn = isbn;
+        }
 }
--- src/main/java/br/com/fiap/spring_mvc/model/Livro.java
+++ src/main/java/br/com/fiap/spring_mvc/model/Livro.java
@@ -1,15 +1,11 @@
 package br.com.fiap.spring_mvc.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "TB_LIVRO")
-@Data
 public class Livro {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,4 +21,52 @@ public class Livro {
     private BigDecimal preco;
     @Column(name = "isbn")
     private String isbn;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 }
