99c251160fa0946106ce48ae4be8a889264ff0ed
Luana Vieira <luanavieira.uni@outlook.com>
2025-03-18 09:48:04 -0300

    :sparkles: feat: adicionada internacionalizaçao nas páginas

--- src/main/java/br/com/fiap/spring_mvc/configuration/InternacionalizacaoConfiguration.java
+++ src/main/java/br/com/fiap/spring_mvc/configuration/InternacionalizacaoConfiguration.java
@@ -1,32 +1,46 @@
 package br.com.fiap.spring_mvc.configuration;
 
-import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Locale;
 
-@Configuration
-public class InternacionalizacaoConfiguration extends AcceptHeaderLocaleResolver {
-    @Override
-    public Locale resolveLocale(HttpServletRequest request) {
-        List<Locale> locales = Arrays.asList(new Locale("br"), new Locale("en"));
-        String headerLanguage = request.getHeader("Accept-Language");
-        return headerLanguage == null || headerLanguage.isEmpty() ?
-                Locale.getDefault() : Locale.lookup(Locale.LanguageRange.parse(headerLanguage), locales);
-    }
 
-    @Bean
-    ResourceBundleMessageSource messageSource() {
-        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
-        source.setBasename("internationalization/messages");
-        source.setDefaultEncoding(StandardCharsets.UTF_8.name());
-        source.setDefaultLocale(new Locale("en"));
-        return source;
-    }
+@Configuration
+public class InternacionalizacaoConfiguration implements WebMvcConfigurer {
+   @Bean
+    public LocaleResolver localeResolver(){
+       SessionLocaleResolver slr = new SessionLocaleResolver();
+       slr.setDefaultLocale(new Locale("pt", "BR"));
+       return slr;
+   }
+
+   @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+       LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+       lci.setParamName("lang");
+       return lci;
+   }
+
+   @Override
+    public void addInterceptors(InterceptorRegistry registry){
+       registry.addInterceptor(localeChangeInterceptor());
+   }
+
+   @Bean
+    public MessageSource messageSource(){
+       ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+       messageSource.setBasename("internationalization/messages");
+       messageSource.setDefaultEncoding(StandardCharsets.ISO_8859_1.name());
+       return messageSource;
+   }
 }
