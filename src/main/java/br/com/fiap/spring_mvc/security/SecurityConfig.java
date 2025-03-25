8afaf2251e8c55029d6a0b194395bda527555993
Luana Vieira <luanavieira.uni@outlook.com>
2025-03-25 09:45:24 -0300

    :lock: security: adicionada autenticação

---
+++ src/main/java/br/com/fiap/spring_mvc/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package br.com.fiap.spring_mvc.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(authorizeRequests -> {
+            authorizeRequests.requestMatchers("/").permitAll();
+            authorizeRequests.anyRequest().authenticated();
+        })
+                .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("/livro/lista"))
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+}
